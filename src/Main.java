25b5f1d2f47abe4354cc58671d89c5dc0e87db8d
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-09-20 12:35:48 +0300

    added report.md and change src

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4508578348053871";
+        String number = "4929238188065554019";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
